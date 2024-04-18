c10aa7fa6b72896fa82b5c3e2b1ba8975705450c
Milko Venkov <MVenkov@infragistics.com>
2024-04-18 15:11:07 +0300

    feat(angular): set up input group

--- angular/src/app/input-group/input-group.component.ts
+++ angular/src/app/input-group/input-group.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IGX_INPUT_GROUP_DIRECTIVES, IgxDateTimeEditorModule } from '@infragistics/igniteui-angular';
+import { IGX_BUTTON_GROUP_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxDateTimeEditorModule } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-input-group',
   standalone: true,
-  imports: [IGX_INPUT_GROUP_DIRECTIVES, IgxDateTimeEditorModule, FormsModule],
+  imports: [IGX_BUTTON_GROUP_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxDateTimeEditorModule, FormsModule, DatePipe],
   templateUrl: './input-group.component.html',
   styleUrls: ['./input-group.component.scss']
 })
 export class InputGroupComponent {
-  public value: string = 'Text';
-  public value1: number = 500;
-  public value2: string = 'test@test.dev';
-  public value3: string = 'Password';
-  public value4: string = 'Phone';
-  public date: Date = new Date('2010-10-10T00:00');
+  public textVar: string | undefined;
+  public numberVar: number | undefined;
+  public email: string | undefined;
+  public password: string | undefined;
+  public phone: string | undefined;
+
+  @Input()
+  public date: Date | undefined;
+
+  textInputChange = (e: Event) => {
+    this.textVar = (e.target as HTMLInputElement).value;
+  }
+
+  numberInputChange = (e: Event) => {
+    // Note the + sign - it is added to cast the string value to number :(
+    this.numberVar = +(e.target as HTMLInputElement).value;
+  }
+
+  emailInputChange = (e: Event) => {
+    this.email = (e.target as HTMLInputElement).value;
+  }
+
+  passwordInputChange = (e: Event) => {
+    this.password = (e.target as HTMLInputElement).value;
+  }
+
+  phoneInputChange = (e: Event) => {
+    this.phone = (e.target as HTMLInputElement).value;
+  }
+
+  dateInputChange = (e: Event) => {
+    this.date = new Date((e.target as HTMLInputElement).value);
+  }
+
+  buttonSetDate = () => this.date = new Date();
 }
