a67bb2c8d8724f607910b5037cf06f659e3bf8a6
Milko Venkov <MVenkov@infragistics.com>
2024-04-18 16:36:43 +0300

    feat(angular): other inputs done

--- angular/src/app/other-inputs/other-inputs.component.ts
+++ angular/src/app/other-inputs/other-inputs.component.ts
@@ -1,7 +1,27 @@
 import { NgFor } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IgcFormsModule, IGX_CALENDAR_DIRECTIVES, IGX_COMBO_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IgxCheckboxComponent, IgxRadioComponent, IgxRadioGroupDirective, IgxSwitchComponent } from '@infragistics/igniteui-angular';
+import {
+  IChangeCheckboxEventArgs,
+  IComboSelectionChangingEventArgs,
+  IgcFormsModule,
+  IGX_CALENDAR_DIRECTIVES,
+  IGX_COMBO_DIRECTIVES,
+  IGX_DATE_PICKER_DIRECTIVES,
+  IGX_INPUT_GROUP_DIRECTIVES,
+  IGX_SELECT_DIRECTIVES,
+  IGX_SIMPLE_COMBO_DIRECTIVES,
+  IGX_SLIDER_DIRECTIVES,
+  IgxCheckboxComponent,
+  IgxRadioComponent,
+  IgxRadioGroupDirective,
+  IgxSwitchComponent,
+  IRangeSliderValue,
+  ISelectionEventArgs,
+  ISimpleComboSelectionChangingEventArgs,
+  ISliderValueChangeEventArgs,
+  IViewDateChangeEventArgs
+} from '@infragistics/igniteui-angular';
 import { defineComponents, IgcRatingComponent } from 'igniteui-webcomponents';
 import { Subject, takeUntil } from 'rxjs';
 import { CategoriesType } from '../models/northwind/categories-type';
@@ -12,18 +32,58 @@ defineComponents(IgcRatingComponent);
 @Component({
   selector: 'app-other-inputs',
   standalone: true,
-  imports: [IGX_SIMPLE_COMBO_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_CALENDAR_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IGX_COMBO_DIRECTIVES, IgxRadioGroupDirective, IgxCheckboxComponent, IgxRadioComponent, IgcFormsModule, IgxSwitchComponent, FormsModule, NgFor, FormsModule],
+  imports: [IGX_SIMPLE_COMBO_DIRECTIVES,
+    IGX_INPUT_GROUP_DIRECTIVES,
+    IGX_DATE_PICKER_DIRECTIVES,
+    IGX_CALENDAR_DIRECTIVES,
+    IGX_SELECT_DIRECTIVES,
+    IGX_SLIDER_DIRECTIVES,
+    IGX_COMBO_DIRECTIVES,
+    IgxRadioGroupDirective,
+    IgxCheckboxComponent,
+    IgxRadioComponent,
+    IgcFormsModule,
+    IgxSwitchComponent,
+    FormsModule,
+    NgFor,
+    FormsModule
+  ],
   templateUrl: './other-inputs.component.html',
   styleUrls: ['./other-inputs.component.scss']
 })
 export class OtherInputsComponent implements OnInit, OnDestroy {
   private destroy$: Subject<void> = new Subject<void>();
   public northwindCategories: CategoriesType[] = [];
-  public value: Date = new Date('2024-04-17T00:00');
-  public value1: string = '1';
-  public value2: number = 3;
-  public value3: number = 60;
-  public value4: string = '{ lower: 20, upper: 80 }';
+  public checked: boolean | undefined;
+  public categoryId: number | undefined;
+  public categoriesIds: number[] = [];
+  public date!: Date;
+  public radioGroupValue: string | undefined;
+  public ratingValue: number | undefined;
+  public sliderValue!: number;
+  public rangeSliderValue!: IRangeSliderValue;
+
+  checkboxChange = (e: IChangeCheckboxEventArgs) => this.checked = e.checked;
+
+  simpleComboSelectionChanging = (e: ISimpleComboSelectionChangingEventArgs) => this.categoryId = e.newValue;
+
+  comboSelectionChanging = (e: IComboSelectionChangingEventArgs) => this.categoriesIds = e.newValue;
+
+  datePickerValueChange = (e: Date) => this.date = e;
+
+  ratingChange = (e: CustomEvent) => this.ratingValue = e.detail;
+
+  selectSelectionChanging = (e: ISelectionEventArgs) => this.categoryId = e.newSelection.value;
+
+  radioGroupChange = (e: IChangeCheckboxEventArgs) => this.radioGroupValue = e.value;
+
+  sliderValueChange = (e: ISliderValueChangeEventArgs) => this.sliderValue = e.value as number; // explicit cast is needed
+
+  rangeSliderValueChange = (e: ISliderValueChangeEventArgs) => this.rangeSliderValue = e.value as IRangeSliderValue; // explicit cast is needed
+
+  switchChange = (e: IChangeCheckboxEventArgs) => this.checked = e.checked;
+
+  calendarViewDateChange = (e: Date | Date[]) => this.date = e as Date;
 
   constructor(
     private northwindService: NorthwindService,
--- angular/src/app/services/northwind.service.ts
+++ angular/src/app/services/northwind.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Northwind } from '../static-data/northwind';
+import { CategoriesType } from '../models/northwind/categories-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NorthwindService {
-  public getData(tableName: string): Observable<any> {
+  public getData(tableName: string): Observable<CategoriesType[]> {
     return of(Northwind[tableName]);
   }
 }
