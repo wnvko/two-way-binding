a0712c4ab65c094ada92d831a56b4d9d65cbd0d7
Milko Venkov <MVenkov@infragistics.com>
2024-09-13 08:15:24 +0300

    chore(angular): ng update @angular/core WIP

--- angular/src/app/other-inputs/other-inputs.component.spec.ts
+++ angular/src/app/other-inputs/other-inputs.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { IgxCheckboxComponent, IGX_SIMPLE_COMBO_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_COMBO_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IgxRadioComponent, IgxRadioGroupDirective, IgcFormsModule, IGX_SELECT_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IgxSwitchComponent, IGX_CALENDAR_DIRECTIVES } from '@infragistics/igniteui-angular';
 import { OtherInputsComponent } from './other-inputs.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('OtherInputsComponent', () => {
   let component: OtherInputsComponent;
@@ -12,9 +13,10 @@ describe('OtherInputsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OtherInputsComponent ],
-      imports: [ NoopAnimationsModule, FormsModule, HttpClientTestingModule, IgxCheckboxComponent, IGX_SIMPLE_COMBO_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_COMBO_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IgxRadioComponent, IgxRadioGroupDirective, IgcFormsModule, IGX_SELECT_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IgxSwitchComponent, IGX_CALENDAR_DIRECTIVES ]
-    })
+    declarations: [OtherInputsComponent],
+    imports: [NoopAnimationsModule, FormsModule, IgxCheckboxComponent, IGX_SIMPLE_COMBO_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_COMBO_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IgxRadioComponent, IgxRadioGroupDirective, IgcFormsModule, IGX_SELECT_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IgxSwitchComponent, IGX_CALENDAR_DIRECTIVES],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+})
     .compileComponents();
   });
 
